1eec1f1e76f87a0fbf2221bbb0b3878f9078aef8
romeocovaci <romeo.covaci@gmail.com>
2019-09-09 00:06:51 +0300

    update component

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Kubee-Sprinks
-Kubee Sprinks custom component ...
+# Kubee Sprinks
+
+## kubee-sprinks
+
+Opensprinkler custom component for Home Assistant
+
+Last tested on OS API `2.1.8` and Home Assistant `0.95.0`
+
+![image](https://user-images.githubusercontent.com/819711/36068687-086820ce-0f2f-11e8-81de-de53c94124f0.png)
+
+### Features
+
+- Binary sensors for each station to show on/off status
+- Binary sensors for OpenSprinkler operation, rain sensor and rain delay
+- Sensors for each station to show status
+- Sensors for water level, last run time and rain delay stop time
+- Programs as scenes which can be "activated"
+- Stations as switches with individual timers
+
+### Installation
+
+1. Copy `custom_components/kubee_sprinks` folder into `<config_dir>/custom_components`
+2. Add the following into your `configuration.yaml`
+    ```yaml
+    kubee_sprinks:
+      host: <host>
+      password: <md5-password>
+    ```
+    - Replace `<host>` with the IP address or hostname of your Opensprinkler controller.
+    - Replace `<password>` with the MD5 encrypted version of your password to your Opensprinkler interface.
+
+3. Restart Home Assistant
+
+### Configuration
+
+- `stations` - by default the component will retrieve all stations but you can limit which stations to show by providing a list of station indexes (starting from 0)
+    ```yaml
+    kubee_sprinks:
+      ...
+      stations:
+        - 0
+        - 3
+        - 4
+    ```
+
+- `programs` - by default the component will retrieve all programs but you can limit which programs to show by providing a list of program indexes (starting from 0)
+    ```yaml
+    kubee_sprinks:
+      ...
+      programs:
+        - 0
+        - 3
+    ```
+
+## Support for HACS
+
+This custom component can be tracked with the help of [HACS](https://github.com/custom-components/hacs).
+
+## Troubleshooting
+
+#### My sprinkler switches are not working
+
+Some users have reported that thier switches do not work. This is due to some `input_number` entities not being created by the component. To get around this issue you can manually create these entities by adding the following for each of your stations.
+
+```yaml
+input_number:
+  <station_name>_timer:
+    min: 1
+    max: 30
+    step: 1
+    mode: slider
+```
+
+Replace `<station_name` with your station name, it should match the names created for the `sensor`, `switch` and `binary_sensor`.
---
+++ custom_components/kubee_sprinks/__init__.py
@@ -0,0 +1,241 @@
+import logging
+import requests
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
+from homeassistant.setup import setup_component
+from homeassistant.const import CONF_HOST, CONF_PASSWORD
+from homeassistant.helpers.discovery import load_platform
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.util import slugify
+
+DOMAIN = 'kubee_sprinks'
+
+VERSION = '0.1.3'
+
+CONF_STATIONS = 'stations'
+CONF_PROGRAMS = 'programs'
+CONF_WATER_LEVEL = 'water_level'
+CONF_LAST_RUN = 'last_run'
+CONF_ENABLE_OPERATION = 'enable_operation'
+CONF_RAIN_DELAY = 'rain_delay'
+CONF_RAIN_DELAY_STOP_TIME = 'rain_delay_stop_time'
+CONF_RAIN_SENSOR = 'rain_sensor'
+CONF_CONFIG = 'config'
+
+_LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = vol.Schema({
+  DOMAIN: vol.Schema({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_STATIONS, default=[]):
+      vol.All(cv.ensure_list, [vol.Coerce(int)]),
+    vol.Optional(CONF_PROGRAMS, default=[]):
+      vol.All(cv.ensure_list, [vol.Coerce(int)]),
+  })
+}, extra=vol.ALLOW_EXTRA)
+
+def setup(hass, config):
+  host = config[DOMAIN].get(CONF_HOST)
+  password = config[DOMAIN].get(CONF_PASSWORD)
+  opensprinkler = Opensprinkler(host, password)
+  stationIndexes = config[DOMAIN].get(CONF_STATIONS)
+
+  hass.data[DOMAIN] = {
+    DOMAIN: opensprinkler,
+    CONF_CONFIG: {
+      CONF_STATIONS: stationIndexes,
+      CONF_PROGRAMS: config[DOMAIN].get(CONF_PROGRAMS),
+    },
+  }
+
+  component = EntityComponent(_LOGGER, 'input_number', hass)
+  inputNumberConfig = {'input_number': {}}
+  for station in opensprinkler.stations():
+    if len(stationIndexes) == 0 or (station.index in stationIndexes):
+      object_id = '{}_timer'.format(slugify(station.name))
+      name = station.name
+      minimum = 1
+      maximum = 10
+      initial = 1
+      step = 1
+      unit = 'minutes'
+
+      inputNumberConfig['input_number'][object_id] = {
+        'min': minimum,
+        'max': maximum,
+        'name': name,
+        'step': step,
+        'initial': 1,
+        'unit_of_measurement': unit,
+      }
+
+  setup_component(hass, 'input_number', inputNumberConfig)
+
+  load_platform(hass, 'binary_sensor', DOMAIN, {}, config)
+  load_platform(hass, 'sensor', DOMAIN, {}, config)
+  load_platform(hass, 'scene', DOMAIN, {}, config)
+  load_platform(hass, 'switch', DOMAIN, {}, config)
+
+  return True
+
+
+class Opensprinkler(object):
+  """ API interface to OpenSprinkler
+
+  For firmware API details, see
+  https://openthings.freshdesk.com/support/solutions/articles/5000716363-os-api-documents
+  """
+
+  def __init__(self, host, password):
+    self._host = host
+    self._password = password
+    self.data = {}
+
+  def _get_controller_variable(self, key, variable):
+    try:
+      url = 'http://{}/jc?pw={}'.format(self._host, self._password)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+    self.data[key] = response.json()[variable]
+
+    return self.data[key]
+
+  def stations(self):
+    try:
+      url = 'http://{}/jn?pw={}'.format(self._host, self._password)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+    self.data[CONF_STATIONS] = []
+
+    for i, name in enumerate(response.json()['snames']):
+      self.data[CONF_STATIONS].append(OpensprinklerStation(self._host, self._password, name, i))
+
+    return self.data[CONF_STATIONS]
+
+  def programs(self):
+    try:
+      url = 'http://{}/jp?pw={}'.format(self._host, self._password)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+    self.data[CONF_PROGRAMS] = []
+
+    for i, data in enumerate(response.json()['pd']):
+      self.data[CONF_PROGRAMS].append(OpensprinklerProgram(self._host, self._password, data[5], i))
+
+    return self.data[CONF_PROGRAMS]
+
+  def water_level(self):
+    try:
+      url = 'http://{}/jo?pw={}'.format(self._host, self._password)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+    self.data[CONF_WATER_LEVEL] = response.json()['wl']
+
+    return self.data[CONF_WATER_LEVEL]
+
+  def last_run(self):
+    return self._get_controller_variable(CONF_LAST_RUN, 'lrun')
+
+  def enable_operation(self):
+    return self._get_controller_variable(CONF_ENABLE_OPERATION, 'en')
+
+  def rain_delay(self):
+    return self._get_controller_variable(CONF_RAIN_DELAY, 'rd')
+
+  def rain_delay_stop_time(self):
+    return self._get_controller_variable(CONF_RAIN_DELAY, 'rdst')
+
+  def rain_sensor(self):
+    return self._get_controller_variable(CONF_RAIN_SENSOR, 'rs')
+
+
+class OpensprinklerStation(object):
+
+  def __init__(self, host, password, name, index):
+    self._host = host
+    self._password = password
+    self._name = name
+    self._index = index
+
+  @property
+  def name(self):
+    return self._name
+
+  @property
+  def index(self):
+    return self._index
+
+  def status(self):
+    try:
+      url = 'http://{}/js?pw={}'.format(self._host, self._password)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+    return response.json()['sn'][self._index]
+
+  def p_status(self):
+    try:
+      url = 'http://{}/jc?pw={}'.format(self._host, self._password)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+    return response.json()['ps'][self._index]
+
+  def turn_on(self, minutes):
+    try:
+      url = 'http://{}/cm?pw={}&sid={}&en=1&t={}'.format(self._host, self._password, self._index, minutes * 60)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+  def turn_off(self):
+    try:
+      url = 'http://{}/cm?pw={}&sid={}&en=0'.format(self._host, self._password, self._index)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
+
+
+class OpensprinklerProgram(object):
+
+  def __init__(self, host, password, name, index):
+    self._host = host
+    self._password = password
+    self._name = name
+    self._index = index
+
+  @property
+  def name(self):
+    return self._name
+
+  @property
+  def index(self):
+    return self._index
+
+  def activate(self):
+    try:
+      url = 'http://{}/mp?pw={}&pid={}&uwt=0'.format(self._host, self._password, self._index)
+      response = requests.get(url, timeout=10)
+      response.encoding = response.apparent_encoding
+    except requests.exceptions.ConnectionError:
+      _LOGGER.error("No route to device '%s'", self._resource)
